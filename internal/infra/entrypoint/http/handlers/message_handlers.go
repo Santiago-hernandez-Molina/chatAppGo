45ca3789498c5f0c028e151070508e973eb81bff
santiagohm <hermolsan@gmail.com>
2024-01-08 20:17:25 -0500

    tabs

--- internal/infra/entrypoint/http/handlers/message_handlers.go
+++ internal/infra/entrypoint/http/handlers/message_handlers.go
@@ -1,44 +1,44 @@
 package handlers
 
 import (
-        "net/http"
-        "strconv"
+	"net/http"
+	"strconv"
 
-        "github.com/Santiago-hernandez-Molina/chatAppBackend/internal/domain/ports"
-        "github.com/gin-gonic/gin"
+	"github.com/Santiago-hernandez-Molina/chatAppBackend/internal/domain/ports"
+	"github.com/gin-gonic/gin"
 )
 
 type MessageHandler struct {
-        messageUseCase ports.MessageUseCase
+	messageUseCase ports.MessageUseCase
 }
 
 func (mh *MessageHandler) GetMessagesByRoomId(ctx *gin.Context) {
-        roomParam := ctx.Param("roomid")
-        roomid, err := strconv.Atoi(roomParam)
-        if err != nil {
-                ctx.JSON(http.StatusBadRequest, gin.H{
-                        "message": "cannot read de room id",
-                })
-                return
-        }
+	roomParam := ctx.Param("roomid")
+	roomid, err := strconv.Atoi(roomParam)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "cannot read de room id",
+		})
+		return
+	}
 
-        messages, err := mh.messageUseCase.GetMessagesByRoomId(roomid)
-        if err != nil {
-                if err != nil {
-                        ctx.JSON(http.StatusInternalServerError, gin.H{
-                                "message": "cannot find the room",
-                        })
-                        return
-                }
-        }
+	messages, err := mh.messageUseCase.GetMessagesByRoomId(roomid)
+	if err != nil {
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "cannot find the room",
+			})
+			return
+		}
+	}
 
-        ctx.JSON(http.StatusOK, messages)
+	ctx.JSON(http.StatusOK, messages)
 }
 
 var _ ports.MessageHandler = (*MessageHandler)(nil)
 
 func NewMessageHandler(messageService ports.MessageUseCase) *MessageHandler {
-        return &MessageHandler{
-                messageUseCase: messageService,
-        }
+	return &MessageHandler{
+		messageUseCase: messageService,
+	}
 }
