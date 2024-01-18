23415118e025b2beb412f33331c8193c6bc122fa
santiagohm <hermolsan@gmail.com>
2024-01-18 14:42:23 -0500

    login

--- test/contact_test.go
+++ test/contact_test.go
@@ -48,6 +48,7 @@ func TestContact(t *testing.T) {
 	t.Run("Send Request", func(t *testing.T) {
 		for _, request := range ContactTestCase {
 			w := MakeRequest[map[string]int]("POST", "/v1/contact/new", request.Args, true, LoginUser)
+
 			assert.Equal(t, request.Status, w.Code)
 		}
 	})
@@ -55,6 +56,7 @@ func TestContact(t *testing.T) {
 		w := MakeRequest[map[string]string]("POST", "/v1/contact/find/sended", nil, true, LoginUser)
 		var response []map[string]any
 		json.Unmarshal(w.Body.Bytes(), &response)
+
 		assert.Equal(t, 200, w.Code)
 		assert.Equal(t, 2, len(response))
 	})
@@ -62,25 +64,34 @@ func TestContact(t *testing.T) {
 		w := MakeRequest[map[string]string]("POST", "/v1/contact/find/received", nil, true, LoginUser2)
 		var response []map[string]any
 		json.Unmarshal(w.Body.Bytes(), &response)
+
 		assert.Equal(t, 200, w.Code)
 		assert.Equal(t, 1, len(response))
 	})
 	t.Run("Accept Request", func(t *testing.T) {
 		w := MakeRequest[map[string]string]("POST", "/v1/contact/accept/0", nil, true, LoginUser2)
+
 		assert.Equal(t, 202, w.Code)
 	})
-	t.Run("View Contact Room", func(t *testing.T) {
+	t.Run("Accept Request (Return 404)", func(t *testing.T) {
+		w := MakeRequest[map[string]string]("POST", "/v1/contact/accept/10", nil, true, LoginUser2)
+
+		assert.Equal(t, 404, w.Code)
+	})
+	t.Run("View Contact Room first user", func(t *testing.T) {
 		w := MakeRequest[map[string]string]("GET", "/v1/room/show/1", nil, true, LoginUser2)
-		assert.Equal(t, 200, w.Code)
 		var response map[string]any
 		json.Unmarshal(w.Body.Bytes(), &response)
+		
+		assert.Equal(t, 200, w.Code)
 		assert.Equal(t, "Juan", response["name"])
 	})
-	t.Run("View Contact Room 2", func(t *testing.T) {
+	t.Run("View Contact Room second user", func(t *testing.T) {
 		w := MakeRequest[map[string]string]("GET", "/v1/room/show/1", nil, true, LoginUser)
-		assert.Equal(t, 200, w.Code)
 		var response map[string]any
 		json.Unmarshal(w.Body.Bytes(), &response)
+
+		assert.Equal(t, 200, w.Code)
 		assert.Equal(t, "Pedro", response["name"])
 	})
 }
